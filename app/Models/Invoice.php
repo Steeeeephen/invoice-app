22cc4e50d95ff35a4f5cc7537487e799d9150343
Stephen Zalalas <stephenzalalas@proton.me>
2026-02-24 21:25:28 -0500

    Add invoice status logic.

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -34,6 +34,29 @@ class Invoice extends Model
                 $invoice->saveQuietly(); // Save without triggering events again
             }
         });
+
+        // Adding some logic here to sync invoice status depending on the amount_due column... hopefully this doesn't come back to haunt me...
+        // Note: the order of these conditions is intentional.
+        // paid/cancelled must be checked before partially_paid to avoid conflicts.
+        static::updating(function ($invoice) {
+            // If the invoice status is either paid or cancelled, amount_due will go to 0.
+            // Also keeping in mind that status will not be changed by a dropdown menu,
+            // invoice status will only be changed based on specific actions... payments, clicking cancel, etc...
+            if (in_array($invoice->status, ['paid', 'cancelled'])) {
+                $invoice->amount_due = 0;
+            }
+
+            // When status is changed to paid, and if paid_at is empty, paid_at will be filled with a timestamp at the current time.
+            if ($invoice->status === 'paid' && empty($invoice->paid_at)) {
+                $invoice->paid_at = now();
+            }
+
+            // Finally in cases of partial payment, if amount_due is less than the amount of the invoice but greater than 0,
+            // status becomes partially_paid.
+            if ($invoice->amount_due > 0 && $invoice->amount_due < $invoice->invoice_amount) {
+                $invoice->status = 'partially_paid';
+            }
+        });
     }
 
 
