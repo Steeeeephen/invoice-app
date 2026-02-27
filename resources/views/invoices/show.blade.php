7127bfb498397376af0fa70834a0d47551745f85
Stephen Zalalas <stephenzalalas@proton.me>
2026-02-26 21:45:57 -0500

    Update style on single invoice view.

--- resources/views/invoices/show.blade.php
+++ resources/views/invoices/show.blade.php
@@ -1,111 +1,122 @@
 <x-layout>
     <div class="flex flex-col container mx-auto px-6 py-6">
-        <div
-            class="flex justify-between items-center bg-slate-700 p-2 shadow-md rounded-md overflow-hidden"
+        {{--      Invoice Header      --}}
+        <section
+            class="flex justify-between items-center rounded-t-2xl bg-slate-700 p-2 shadow-md overflow-hidden"
         >
             <h1 class="text-6xl font-bold text-gray-100">
                 {{ $invoice->invoice_number }}
             </h1>
 
-            @can("update", $invoice)
-                <a
-                    href="{{ route("invoices.edit", $invoice->id) }}"
-                    class="bg-violet-900 font-bold rounded p-2 text-white hover:bg-violet-700 cursor-pointer"
-                >
-                    Edit
-                </a>
-            @endcan
-
-            @if ($invoice->status === "draft")
-                <form
-                    action="{{ route("invoices.send", $invoice) }}"
-                    method="POST"
-                >
-                    @csrf
-                    @method("PATCH")
-                    <button
-                        class="bg-violet-900 font-bold rounded p-2 text-white hover:bg-violet-700 cursor-pointer"
-                        type="submit"
+            <div class="flex gap-4 items-center">
+                @can("update", $invoice)
+                    <a
+                        href="{{ route("invoices.edit", $invoice->id) }}"
+                        class="bg-purple-900 font-bold rounded text-white hover:bg-purple-700 transition-all ease-in cursor-pointer px-4 py-2 w-32 text-center"
                     >
-                        SEND
-                    </button>
-                </form>
-            @endif
+                        Edit
+                    </a>
+                @endcan
 
-            <h2 class="text-2xl">
+                @can("send", $invoice)
+                    <form
+                        action="{{ route("invoices.send", $invoice) }}"
+                        method="POST"
+                    >
+                        @csrf
+                        @method("PATCH")
+                        <button
+                            class="bg-green-700 font-bold rounded text-white hover:bg-green-500 hover:text-gray-900 transition-all ease-in cursor-pointer px-4 py-2 w-32 text-center"
+                            type="submit"
+                        >
+                            SEND
+                        </button>
+                    </form>
+                @endcan
 
-                Status:
-                <x-invoice-status :status="$invoice->status"/>
-            </h2>
-        </div>
+                <h2 class="text-2xl">
+                    Status:
+                    <x-invoice-status :status="$invoice->status"/>
+                </h2>
+            </div>
+        </section>
 
-        <section
-            class="min-w-full bg-slate-800 border border-slate-700 my-6 p-4 shadow-md rounded-md overflow-hidden flex justify-between"
-        >
-            <div>
-                <h3
-                    class="text-lg font-semibold text-gray-400 uppercase tracking-wider mb-2"
-                >
-                    Contact Info
-                </h3>
-                <a
-                    class="text-indigo-400 hover:text-indigo-300"
-                    href="mailto:{{ $invoice->customer->email }}"
-                >
-                    {{ $invoice->customer->email }}
-                </a>
-                <br/>
-                <span class="text-gray-300">
+        <div class="bg-slate-800 rounded-b-3xl">
+
+            {{--      Customer Info      --}}
+            <section
+                class="min-w-full bg-slate-800 my-6 p-4  rounded-md overflow-hidden flex justify-between items-center"
+            >
+                <div>
+                    <a href="{{ route('customers.show', $invoice->customer) }}"><h2
+                            class="text-3xl text-slate-300 hover:text-white">{{$invoice->customer->first_name}} {{ $invoice->customer->last_name}}</h2>
+                    </a>
+                    <h3
+                        class="text-lg font-semibold text-gray-400 uppercase tracking-wider mb-2"
+                    >
+                        Contact Info
+                    </h3>
+                    <a
+                        class="text-indigo-400 hover:text-indigo-300"
+                        href="mailto:{{ $invoice->customer->email }}"
+                    >
+                        {{ $invoice->customer->email }}
+                    </a>
+                    <br/>
+                    <span class="text-gray-300">
                     {{ $invoice->customer->phone }}
                 </span>
-            </div>
+                </div>
 
-            <div>
+                <div>
+                    <h3
+                        class="text-lg font-semibold text-gray-400 uppercase tracking-wider mb-2"
+                    >
+                        Billing Address
+                    </h3>
+                    <p class="text-gray-300">
+                        {{ $invoice->customer->company_name }}
+                        <br/>
+                        {{ $invoice->customer->street_address }}
+                        <br/>
+                        {{ $invoice->customer->city }},
+                        {{ $invoice->customer->state }}
+                        {{ $invoice->customer->zip }}
+                        <br/>
+                    </p>
+                </div>
+            </section>
+
+            <hr class="border-slate-600">
+
+            {{--      Invoice todal, and amount due      --}}
+            <section
+                class="min-w-full bg-slate-800 my-6 p-4  rounded-md overflow-hidden"
+            >
+                <div class="flex justify-between">
+                    <h2 class="text-4xl">
+                        Invoice total:
+                        ${{ number_format($invoice->invoice_amount, 2, ".", ",") }}
+                    </h2>
+                    <h2 class="text-4xl">
+                        Amount due:
+                        ${{ number_format($invoice->amount_due, 2, ".", ",") }}
+                    </h2>
+                </div>
+            </section>
+
+            <hr class="border-slate-600">
+
+            <section
+                class="min-w-full bg-slate-800  my-6 p-4 rounded-md overflow-hidden"
+            >
                 <h3
                     class="text-lg font-semibold text-gray-400 uppercase tracking-wider mb-2"
                 >
-                    Billing Address
+                    Description
                 </h3>
-                <p class="text-gray-300">
-                    {{ $invoice->customer->company_name }}
-                    <br/>
-                    {{ $invoice->customer->street_address }}
-                    <br/>
-                    {{ $invoice->customer->city }},
-                    {{ $invoice->customer->state }}
-                    {{ $invoice->customer->zip }}
-                    <br/>
-                </p>
-            </div>
-        </section>
-
-        <section
-            class="min-w-full bg-slate-800 border border-slate-700 my-6 p-4 shadow-md rounded-md overflow-hidden "
-        >
-            <div class="flex justify-between">
-                <h1 class="text-4xl">
-                    Invoice total:
-                    ${{ number_format($invoice->invoice_amount, 2, ".", ",") }}
-                </h1>
-                <h1 class="text-4xl">
-                    Amount due:
-                    ${{ number_format($invoice->amount_due, 2, ".", ",") }}
-                </h1>
-            </div>
-
-        </section>
-
-        <section
-            class="min-w-full bg-slate-800 border border-slate-700 my-6 p-4 shadow-md rounded-md overflow-hidden"
-        >
-            <h3
-                class="text-lg font-semibold text-gray-400 uppercase tracking-wider mb-2"
-            >
-                Description
-            </h3>
-            <p>{{ $invoice->description }}</p>
-        </section>
-
-        <section></section>
+                <p>{{ $invoice->description }}</p>
+            </section>
+        </div>
     </div>
 </x-layout>
