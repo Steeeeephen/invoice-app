e092f8f46a30a1563175352fa14c6359d461096d
Stephen Zalalas <stephenzalalas@proton.me>
2026-03-01 08:10:52 -0500

    Create users index view

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,53 @@
+<x-layout>
+
+    <div class="flex flex-col container mx-auto px-6 py-6">
+        <section class="flex items-center mb-4 justify-between">
+            <h1 class="text-2xl font-bold mb-4">Admin List</h1>
+            <a href="{{ route('users.create') }}"
+               class="bg-blue-600 text-white text-sm px-4 py-2 rounded hover:bg-blue-700 mr-6">
+                + Create User
+            </a>
+        </section>
+
+        <table
+            class="min-w-full bg-slate-800 border border-slate-700 my-6 rounded-md overflow-hidden shadow-md shadow-black/40">
+            <thead class="bg-slate-700 text-left text-sm uppercase tracking-wider text-gray-400">
+            <tr>
+                <th class="px-6 py-3">Name</th>
+                <th class="px-6 py-3">Email</th>
+                <th class="px-6 py-3">Role</th>
+                <th class="px-6 py-3"></th>
+
+            </tr>
+            </thead>
+            <tbody class="divide-y divide-slate-700">
+            @foreach ($users as $user)
+                <tr class="hover:bg-slate-700/50 transition-colors cursor-pointer"
+                    {{--                    onclick="window.location='{{ route('customers.show', $customer) }}'"--}}
+                >
+                    <td class="px-6 py-4">{{ $user->first_name }} {{ $user->last_name }}</td>
+                    <td class="px-6 py-4">{{ $user->email }}</td>
+                    <td class="px-6 py-4">{{ $user->role }}</td>
+                    <td class="px-6 py-4 text-right">
+                        <a href="{{ route('users.edit', $user->id) }}">
+                            <button
+                                class="bg-blue-600 text-white text-sm px-4 py-2 rounded hover:bg-blue-700 mr-6 cursor-pointer">
+                                Edit
+                            </button>
+                        </a>
+
+                        <button
+                            class="bg-red-600 text-white text-sm px-4 py-2 rounded hover:bg-red-700 mr-6 cursor-pointer">
+                            Delete
+                        </button>
+                    </td>
+
+
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+
+
+</x-layout>
