e9925930d5c369367cecaca8927de0d0c625fa43
Stephen Zalalas <stephenzalalas@proton.me>
2026-03-03 19:02:39 -0500

    Add update and delete user controller.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
@@ -37,6 +38,7 @@ class UserController extends Controller
             'password_confirmation' => 'required',
             'role' => 'required|in:super_admin,admin,client',
         ]);
+
         User::create($incomingFields);
         return redirect()->route('users.index')->with('success', 'User created successfully');
 
@@ -61,16 +63,45 @@ class UserController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, User $user)
     {
-        //
+        $incomingFields = $request->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            // SINCE THIS HAS NOW TRIPPED ME UP TWICE...
+            // There is a slightly different syntax here since I'm adding the rule to ignore the
+            // $users email if it's unchanged...
+            // The original pipe delimited syntax is in the store method above.
+            // Since the Rule:: is added it changes to an array syntax.
+            'email' => [
+                'required',
+                'email',
+                Rule::unique('users', 'email')->ignore($user),
+            ],
+            'password' => 'nullable|confirmed',
+            'role' => 'required|in:super_admin,admin,client',
+        ]);
+
+        if (!empty($incomingFields['password'])) {
+            // If the password field is filled, the password will be hashed and updated.
+            $incomingFields['password'] = bcrypt($incomingFields['password']);
+        } else {
+            // If the password field is empty, it is dropped from the $incomingFields array and the original password is kept.
+            unset($incomingFields['password']);
+        }
+
+        $user->update($incomingFields);
+
+        return redirect()->route('users.index')->with('success', 'User updated successfully');
+
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(User $user)
     {
-        //
+        $user->delete();
+        return redirect()->route('users.index')->with('success', 'User deleted successfully');
     }
 }
