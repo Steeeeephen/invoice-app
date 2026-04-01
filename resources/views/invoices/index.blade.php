740995d68ffeb0383af2e93bd6253c189694b771
Stephen Zalalas <stephenzalalas@proton.me>
2026-04-01 17:37:05 -0400

    Add amount due column to invoices index.

--- resources/views/invoices/index.blade.php
+++ resources/views/invoices/index.blade.php
@@ -15,6 +15,7 @@
                 <th class="px-6 py-3">Invoice Number</th>
                 <th class="px-6 py-3">Customer</th>
                 <th class="px-6 py-3">Company</th>
+                <th class="px-6 py-3">Amount Due</th>
                 <th class="px-6 py-3">Due Date</th>
                 <th class="px-6 py-3">Status</th>
             </tr>
@@ -26,6 +27,7 @@
                     <td class="px-6 py-4">{{ $invoice->invoice_number }}</td>
                     <td class="px-6 py-4">{{ $invoice->customer->first_name }} {{ $invoice->customer->last_name }}</td>
                     <td class="px-6 py-4">{{ $invoice->customer->company_name }}</td>
+                    <td class="px-6 py-4">{{ $invoice->amount_due }}</td>
                     <td class="px-6 py-4">{{ $invoice->due_date }}</td>
                     <td class="px-6 py-4">
                         <x-invoice-status :status="$invoice->status"/>
