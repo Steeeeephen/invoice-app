88dfbfce40766c788b2e46929f02f10d175ec371
Stephen Zalalas <stephenzalalas@proton.me>
2026-04-04 10:26:02 -0400

    Create mail template.

---
+++ app/Mail/InvoiceCreated.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Invoice;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Attachment;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class InvoiceCreated extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(
+        public Invoice $invoice,
+        protected string $pdfData,
+    ){}
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            from: new Address('stephen.zalalas@gmail.com', 'Invoice App Demo'),
+            subject: 'Invoice #' . $this->invoice->invoice_number,
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'mail.invoice-created',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [
+            Attachment::fromData(fn () => $this->pdfData,
+                'invoice-' . $this->invoice->invoice_number . '.pdf'
+            )->withMime('application/pdf'),
+        ];
+    }
+}
